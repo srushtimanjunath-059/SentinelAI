6869d60c74fc6c4bba9a1ea75396b471ab060866
Srushtimanjunath-059 <srushtimanjunath059@gmail.com>
2026-08-01 23:12:54 +0530

    Added analytics, history, dashboard updates and frontend improvements

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,1 +1,27 @@
-console.log("Sentinel AI Started");
+async function analyzeTransaction() {
+
+    let amount =
+    document.getElementById("amount").value;
+
+    let response =
+    await fetch(
+        `http://127.0.0.1:8000/analyze/${amount}`
+    );
+
+    let data = await response.json();
+
+    document.getElementById("risk").innerHTML =
+    data.risk_score + "%";
+
+    if(data.risk_score >= 60){
+
+        document.getElementById("status").innerHTML =
+        "🚨 High Risk Transaction";
+
+    } else {
+
+        document.getElementById("status").innerHTML =
+        "✅ Safe Transaction";
+
+    }
+}
